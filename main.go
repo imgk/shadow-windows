2bd7d55377311609705d37a902777d3b40538a28
John Xiong <imgk@mail.ustc.edu.cn>
2020-09-23 20:46:09 +0800

    windows ui

---
+++ main.go
@@ -0,0 +1,91 @@
+// +build windows
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc/eventlog"
+
+	"github.com/imgk/shadow/windows/pkg"
+	_ "github.com/imgk/shadow/windows/res"
+)
+
+const (
+	ServiceName = "Shadow"
+	ServiceDesc = "A Transparent Proxy for Windows, Linux and macOS"
+)
+
+func main() {
+	isIntSess, err := svc.IsAnInteractiveSession()
+	if err != nil {
+		panic(fmt.Errorf("failed to determine if we are running in an interactive session: %v", err))
+	}
+	if !isIntSess {
+		file := flag.String("c", "config.json", "config file")
+		flag.Parse()
+
+		elog, err := eventlog.Open(ServiceName)
+		if err != nil {
+			panic(err)
+		}
+		defer elog.Close()
+
+		if err := svc.Run(ServiceName, pkg.Service{Log: elog, File: *file}); err != nil {
+			elog.Error(1, fmt.Sprintf("%s service failed: %v", ServiceName, err))
+			return
+		}
+
+		elog.Info(1, fmt.Sprintf("%s service stopped", ServiceName))
+		return
+	}
+
+	if len(os.Args) < 2 {
+		monitor := &pkg.Monitor{
+			ServiceName: ServiceName,
+			ServiceDesc: ServiceDesc,
+		}
+		if err := monitor.Run(); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	switch os.Args[1] {
+	case "/install":
+		conf, err := pkg.GetConfig("config.json")
+		if err != nil {
+			panic(err)
+		}
+		if err := pkg.InstallService(ServiceName, ServiceDesc, []string{"-c", conf}); err != nil {
+			panic(err)
+		}
+		fmt.Println("service installed successfully")
+	case "/remove":
+		if err := pkg.RemoveService(ServiceName); err != nil {
+			panic(err)
+		}
+		fmt.Println("service removed successfully")
+	case "/start":
+		if err := pkg.StartService(ServiceName); err != nil {
+			panic(err)
+		}
+		fmt.Println("service started successfully")
+	case "/stop":
+		if err := pkg.ControlService(ServiceName, svc.Stop, svc.Stopped); err != nil {
+			panic(err)
+		}
+		fmt.Println("service stopped successfully")
+	default:
+		monitor := &pkg.Monitor{
+			ServiceName: ServiceName,
+			ServiceDesc: ServiceDesc,
+		}
+		if err := monitor.Run(); err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ pkg/config.go
@@ -0,0 +1,209 @@
+// +build windows
+
+package pkg
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/imgk/shadow/app"
+)
+
+func GetConfig(conf string) (string, error) {
+	if filepath.IsAbs(conf) {
+		return conf, checkConfig(conf)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		return conf, err
+	}
+	conf = filepath.Join(dir, conf)
+
+	return conf, checkConfig(conf)
+}
+
+func checkConfig(conf string) error {
+	info, err := os.Stat(conf)
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return errors.New("not a file")
+	}
+
+	return nil
+}
+
+func GetRuleDir(conf string) (string, error) {
+	if filepath.IsAbs(conf) {
+		return conf, checkRuleDir(conf)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		return conf, err
+	}
+	conf = filepath.Join(dir, conf)
+
+	return conf, checkRuleDir(conf)
+}
+
+func checkRuleDir(conf string) error {
+	info, err := os.Stat(conf)
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return nil
+	}
+
+	return errors.New("not a dir")
+}
+
+func GetRules(rules string) (s []string, m map[string]string, err error) {
+	dirs, err := ioutil.ReadDir(rules)
+	if err != nil {
+		return
+	}
+
+	m = make(map[string]string)
+	for _, info := range dirs {
+		if info.IsDir() {
+			dir := info.Name()
+
+			files, er := ioutil.ReadDir(filepath.Join(rules, dir))
+			if er != nil {
+				err = er
+				return
+			}
+
+			for _, info := range files {
+				if info.IsDir() {
+					continue
+				}
+				file := info.Name()
+				if strings.HasPrefix(file, ".") {
+					continue
+				}
+
+				file = filepath.Join(dir, file)
+				s = append(s, file)
+				m[file] = filepath.Join(rules, file)
+			}
+			continue
+		}
+
+		file := info.Name()
+		if strings.HasPrefix(file, ".") {
+			continue
+		}
+
+		s = append(s, file)
+		m[file] = filepath.Join(rules, file)
+	}
+	return
+}
+
+func ParseRules(file string) (apps, cidr []string, err error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		l, er := r.ReadString('\n')
+		if er != nil {
+			if errors.Is(er, io.EOF) {
+				break
+			}
+			err = er
+			return
+		}
+		if len(l) == 0 {
+			continue
+		}
+
+		l = strings.TrimSuffix(strings.TrimSuffix(l, "\n"), "\r")
+		if strings.HasPrefix(l, "#") {
+			continue
+		}
+		if strings.HasSuffix(l, ".exe") {
+			apps = append(apps, l)
+			continue
+		}
+		if _, _, er := net.ParseCIDR(l); er == nil {
+			cidr = append(cidr, l)
+		}
+	}
+
+	return
+}
+
+func Generate(server string, apps, cidr []string) (err error) {
+	proxyServer, err := ParseServer(server)
+	if err != nil {
+		return
+	}
+	config, err := GetConfig("config.json")
+	if err != nil {
+		return
+	}
+	b, err := ioutil.ReadFile(config)
+	conf := app.Conf{}
+	if err = json.Unmarshal(b, &conf); err != nil {
+		return
+	}
+	nameServer, err := ParseServer(conf.NameServer)
+	if err != nil {
+		return
+	}
+
+	conf.Server = server
+	conf.FilterString = fmt.Sprintf("outbound and ip and ip.DstAddr != %v and ip.DstAddr != %v", proxyServer, nameServer)
+	conf.IPCIDRRules.Proxy = append(cidr, "198.18.0.0/16")
+	conf.AppRules.Proxy = apps
+
+	f, err := os.OpenFile(config, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	b, err = json.MarshalIndent(&conf, "", "    ")
+	if err != nil {
+		return
+	}
+	if _, err = f.Write(b); err != nil {
+		return
+	}
+	return
+}
+
+func ParseServer(server string) (string, error) {
+	u, err := url.Parse(server)
+	if err != nil {
+		return server, err
+	}
+
+	addr, err := net.ResolveUDPAddr("udp", u.Host)
+	if err != nil {
+		return server, err
+	}
+
+	server, _, err = net.SplitHostPort(addr.String())
+	return server, err
+}
