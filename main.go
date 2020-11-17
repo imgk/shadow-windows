6fc2ee8e150af851a5b4d1ae2851d9650f1d4478
John <xiaoyang1258@yeah.net>
2020-11-17 06:09:36 +0000

    main.go: remove resource

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	//protocols
-	_ "github.com/imgk/shadow-windows/resource"
 	_ "github.com/imgk/shadow/protocol/http"
 	_ "github.com/imgk/shadow/protocol/shadowsocks"
 	_ "github.com/imgk/shadow/protocol/socks"
